01f91a94cda0f9c4f62d56e2ad9f0e0f1995f0e8
Kim Ngan <trinhngan.sof@gmail.com>
2025-04-25 00:46:25 +0700

    fix: remove prop-types from external build config

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,10 @@ import react from "@vitejs/plugin-react-swc";
 import path from "node:path";
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./",
   build: {
     rollupOptions: {
-      external: ["prop-types"],
+      // external: ["prop-types"],
     },
     outDir: "dist",
     chunkSizeWarningLimit: 1000,
